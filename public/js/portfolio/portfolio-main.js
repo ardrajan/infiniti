9480619069d5722746b9e00d25cf635cf58e8a77
ardrajan <ard.rajan@gmail.com>
2015-09-07 00:13:54 -0400

    One journey

--- public/js/portfolio/portfolio-main.js
+++ public/js/portfolio/portfolio-main.js
@@ -1,5 +1,16 @@
 (function ($) {
+    // Custom transform and opacity modifier for Stellar.js
+    $.stellar.positionProperty.transfade = {
+            setPosition: function(element, newLeft, originalLeft, newTop, originalTop) {
+            var distance = newTop - originalTop;
+            var rate = $(window).height() / 5;
+            element.css('transform', 'translate3d(0, ' + distance + 'px, 0').css('opacity', 1 - (distance / rate));
+        }
+    };
+
     $.stellar({
-        horizontalOffset: 40
+        horizontalOffset: 50,
+        verticalOffset: 150,
+        positionProperty: 'transfade',
     });
 }(jQuery));
