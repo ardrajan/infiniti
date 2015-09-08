87e375bff5b87b737417b2a6a5ae98c28cd292c6
ardrajan <ard.rajan@gmail.com>
2015-09-07 22:04:11 -0400

    Adding handlebars

--- public/js/portfolio/portfolio-main.js
+++ public/js/portfolio/portfolio-main.js
@@ -13,4 +13,32 @@
         verticalOffset: 150,
         positionProperty: 'transfade',
     });
+
+    // Get the JSON
+    var projectData;
+    $.getJSON('public/data/projects.json', function(data) {
+        projectData = data;
+    });
+    console.log(projectData);
+
+    // Grab the template script
+    var theTemplateScript = $("#project-card-template").html();
+
+    // Compile the template
+    var theTemplate = Handlebars.compile(theTemplateScript);
+
+    // Define our data object
+    // var context={
+    // "city": "London",
+    // "street": "Baker Street",
+    // "number": "221B"
+    // };
+
+    // // Pass our data to the template
+    // var theCompiledHtml = theTemplate(context);
+
+    // Add the compiled html to the page
+    // $('.content-placeholder').html(theCompiledHtml);
+    $('.projects-grid').html(theTemplate);
+
 }(jQuery));
