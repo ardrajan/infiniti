a2fd2e2f3e56036bd83333b44bae1d75e51e5c53
ardrajan <ard.rajan@gmail.com>
2015-09-14 19:02:50 -0400

    Added content to case study

--- public/js/portfolio/portfolio-main.js
+++ public/js/portfolio/portfolio-main.js
@@ -18,6 +18,8 @@
     var projectsGrid = $('.projects-grid');
     var modalTitle = $('.modal-title');
     var modalBody = $('.modal-body');
+    var caseStudySection = $('#case-study');
+    var modalCloseButton = $('.close');
 
     // Get the JSON data
     var jsonURL = 'public/data/projects.json';
@@ -43,7 +45,7 @@
         // Add the compiled html to the page
         projectsGrid.html(projectCardCompiledHtml);
 
-        // On click event for getting JSON data corresponding clicked element
+        // Click event for getting JSON data corresponding clicked element
         projectsGrid.on('click', '.project-card', function(e) {
             // Get the index of the parent of the clicked element
             var index = $(this).parent().index();
@@ -63,6 +65,17 @@
 
             // Add the compiled html to the modal body
             modalBody.html(projectCardDetailsCompiledHtml);
+        });
+    });
+
+    // Click event for attaching the case study details when the case study button is clicked
+    $('#case-study-button').on('click', function() {
+        modalTitle.text('mCLASS:CIRCLE');
+        caseStudySection.show();
+        modalBody.html(caseStudySection);
+        modalCloseButton.on('click', function() {
+            caseStudySection.hide();
+            $(this).off('click');
         });
     });
 }(jQuery));
