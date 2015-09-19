e1f9947b29bd0393b5ff26a6cb12c71d41c6effd
ardrajan <ard.rajan@gmail.com>
2015-09-19 16:44:19 -0400

    Removed stellar.js

--- public/js/portfolio/portfolio-main.js
+++ public/js/portfolio/portfolio-main.js
@@ -1,19 +1,4 @@
 (function ($) {
-    // Custom transform and opacity modifier for Stellar.js
-    $.stellar.positionProperty.transfade = {
-            setPosition: function(element, newLeft, originalLeft, newTop, originalTop) {
-            var distance = newTop - originalTop;
-            var rate = $(window).height() / 5;
-            element.css('transform', 'translate3d(0, ' + distance + 'px, 0').css('opacity', 1 - (distance / rate));
-        }
-    };
-
-    $.stellar({
-        horizontalOffset: 50,
-        verticalOffset: 150,
-        positionProperty: 'transfade',
-    });
-
     // Variables for optimization
     var projectsGrid = $('.projects-grid');
     var modalTitle = $('.modal-title');
